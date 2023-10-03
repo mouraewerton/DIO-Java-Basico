f10c441ac838d5999f8a181783e32233c50f6c71
Bowie <ewerton.moura@live.com>
2023-10-03 20:41:39 -0300

    Metodos1

--- Metodos/src/Metodos.java
+++ Metodos/src/Metodos.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Métodos - Exemplos para nomenclatura de métodos
+        // somar(int n1, int n2){}
+
+        // abrirConexao(){}
+
+        // concluirProcessamento(){}
+
+        // findById(int id){} // normalmente encontraremos vários metodos em outra
+        // lingua.
+
+        // calcularImprimir(){} // há algo errado neste método, ou ele conclui uma ação
+        // ou outra, não há como calcular e imprimir no mesmo método.
+
+        // **Caso o método não retorne nenhum valor, ele será representado pela
+        // palavra-chave "void".**
+        // Quais os parametros serão necessários para execução do método?
+        // O metodo possui o risco de apresentar alguma exceção?
+        // Qual a visibilidade do método?
+
     }
 }
