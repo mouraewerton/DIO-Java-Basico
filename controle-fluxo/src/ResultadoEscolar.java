0f9c6bf9150a17577b897b904f24543aa5f831bc
Bowie <ewerton.moura@live.com>
2023-10-09 06:17:20 -0300

    Commit Resultado Escolar

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -3,11 +3,34 @@ public class ResultadoEscolar {
 
         int nota = 6;
 
-        if (nota >= 5)
+        if (nota >= 7) // Retornar "True"
             System.out.println("Aprovado: " + nota);
+
+        else if (nota >= 5 && nota < 7) // Retornar "True ou False"
+            System.out.println("Exame");
 
         else
             System.out.println("Reprovado: " + nota);
+
+        /*
+         * Condição Ternária
+         * int nota = 7;
+         * 
+         * String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+         * 
+         * System.out.println(resultado);
+         */
+
+        /*
+         * Condição Encadeada
+         * int nota = 7;
+         * 
+         * String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Exame" :
+         * "Reprovado";
+         * 
+         * System.out.println(resultado);
+         * 
+         */
 
     }
 }
