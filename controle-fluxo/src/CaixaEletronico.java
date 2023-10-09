afec08015a85ba205bff2383621972accbb69901
Bowie <ewerton.moura@live.com>
2023-10-09 05:56:54 -0300

    Melhorias

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,11 +1,14 @@
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 25.0;
-        double valorSolicitado = 17.0;
+        double valorSolicitado = 23.0;
 
-        if (valorSolicitado < saldo)
+        if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Saldo Atual: " + saldo);
 
-        System.out.println(saldo);
+        } else
+            System.out.println("Saldo Insuficiente.");
+
     }
 }
---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) throws Exception {
+
+        int nota = 6;
+
+        if (nota >= 5)
+            System.out.println("Aprovado: " + nota);
+
+        else
+            System.out.println("Reprovado: " + nota);
+
+    }
+}
