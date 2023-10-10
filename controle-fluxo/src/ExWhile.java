be9aba7dfc1a33b57cd262620250b9721edfe257
Bowie <ewerton.moura@live.com>
2023-10-10 06:51:09 -0300

    Exemplo While

---
+++ controle-fluxo/src/ExWhile.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExWhile {
+    public static void main(String[] args) throws Exception {
+        double mesada = 50.0;
+
+        while (mesada > 0) {
+            Double valorDoce = valorAleatorio();
+            System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho: ");
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada:" + mesada);
+        System.out.println("Joãozinho gastou toda a sua mesada em doces");
+    }
+
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+}
