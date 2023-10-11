f2ef4faf75cc6f85e7083b0cdc239c409b6bd924
Ewerton Moura <102037361+mouraewerton@users.noreply.github.com>
2023-10-11 15:45:12 -0300

    Atualizando Desafios

---
+++ Desafios-DIO-Java/src/CofresSeguros.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+abstract class Cofre {
+    protected String tipo;
+    protected String metodoAbertura;
+
+    public Cofre(String tipo, String metodoAbertura) {
+        this.tipo = tipo;
+        this.metodoAbertura = metodoAbertura;
+    }
+
+    public void imprimirInformacoes() {
+        System.out.println("Tipo: " + this.tipo);
+        System.out.println("Metodo de abertura: " + this.metodoAbertura);
+    }
+}
+
+class CofreDigital extends Cofre {
+
+    private int senha;
+
+    public CofreDigital(int senha) {
+        super("Cofre Digital", "Senha");
+        this.senha = senha;
+    }
+
+    public boolean validarSenha(int confirmacaoSenha) {
+        return confirmacaoSenha == this.senha;
+    }
+}
+
+class CofreFisico extends Cofre {
+
+    public CofreFisico() {
+        super("Cofre Fisico", "Chave");
+    }
+
+}
+
+public class CofresSeguros {
+    public static void main(String[] args) {
+        // Lê o tipo de cofre (primeira linha da entrada)
+        Scanner scanner = new Scanner(System.in);
+        String tipoCofre = scanner.nextLine();
+
+        // TODO: Implemente a condição necessário para a verificação dos cofres seguros:
+        if (tipoCofre.equalsIgnoreCase("digital")) {
+            int senha = scanner.nextInt();
+            CofreDigital cofreDigital = new CofreDigital(senha);
+            System.out.println("Tipo: Cofre Digital");
+            System.out.println("Metodo de abertura: Senha");
+            int tentativaSenha = scanner.nextInt();
+
+            if (cofreDigital.validarSenha(tentativaSenha)) {
+                System.out.println("Cofre aberto!");
+            } else {
+                System.out.println("Senha incorreta!");
+
+            }
+        } else if (tipoCofre.equalsIgnoreCase("fisico")) {
+            CofreFisico cofreFisico = new CofreFisico();
+            System.out.println("Tipo: Cofre Fisico");
+            System.out.println("Metodo de abertura: Chave");
+        } else {
+            System.out.println("Tipo de cofre inválido.");
+        }
+        scanner.close();
+    }
+}
